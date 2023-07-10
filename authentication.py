49b31e753e30d07837f1078123f959e378312ce2
Nils Becker <nils.becker0711@gmail.com>
2023-07-10 21:14:35 +0200

    #2 focus on 2 hyper parameters

--- authentication.py
+++ authentication.py
@@ -55,18 +55,18 @@ def save_classifier(classifier):
     '''
     This method saves a given classifier to the models subfolder with current datetime as destinction method. The datetime format is D/M/Y_H_M
     :param classifier: classifier to save (As it is only SVC)
-    :return: True if successful, False otherwise
+    :return: path to file
     '''
 
     try:
        now = datetime.now()
-       date = now.strftime("%d/%m/%Y_%H_%M")
+       date = now.strftime("%d_%m_%Y_%H_%M")
        filename = os.path.join(dirname, f'models/svc_model{date}.jl')
        joblib.dump(classifier, filename )
-       return True
+       return filename
     except Exception as e:
          print(e)
-         return False
+         return None
     
 def load_classifier(path, from_models=False):
     '''
@@ -77,7 +77,7 @@ def load_classifier(path, from_models=False):
     '''
 
     if from_models:
-        path = (f"models/{path}")    
+        path = os.path.join(dirname,f"models/{path}")   
     try:
         classifier=joblib.load(path)
         return classifier
@@ -107,10 +107,10 @@ def train_gender_classification(data_path, audio_path):
         genders.append(gender)
 
     #Split Data
-    X_train, X_test, y_train, y_test = train_test_split(features, genders, test_size=0.2)
+  
 
     #Actual Training
-    classifier = train_classifier( X_train, X_test, y_train, y_test)
+    classifier = train_classifier(features, genders)
     return classifier
 
 def train_speaker_classification(data_path, audio_path):
@@ -124,40 +124,43 @@ def train_speaker_classification(data_path, audio_path):
     voice_samples = pd.read_excel(data, usecols=[0,1])
     speakers = []
     features = []
+    i=0
     print(f"Feature Extraction started: {datetime.now()}")
     for index, sample in voice_samples.iterrows():
-        speakerID, audio_path_detail = sample
+        if i==10:
+            pass
+        speaker_ID, audio_path_detail = sample
         mfcc_features, sr, path = extract_mfcc(audio_path+audio_path_detail)
-        speakers.append(speakerID)
-        features.append(mfcc_features)      
+        speakers.append(speaker_ID)
+        features.append(mfcc_features)  
+        i=i+1    
     print(f"Feature Extraction completed: {datetime.now()}")
 
-    X_train, X_test, y_train, y_test = train_test_split(features, speakers, test_size=0.2)
-
-    classifier = train_classifier( X_train, X_test, y_train, y_test)
+    classifier = train_classifier(features, speakers)
     return classifier
 
 
-def train_classifier(X_train, X_test, y_train, y_test):
+def train_classifier(features, criteria):
 
+    X_train, X_test, y_train, y_test = train_test_split(features, criteria, test_size=0.2)
+ 
     classifier = SVC()
+
     param_grid = {
     'C': [0.1, 1, 10],
-    'kernel': ['linear', 'poly', 'rbf'],
-    'gamma': [0.1, 1, 10],
-    'degree': [2, 3, 4],
-    'class_weight': [None, 'balanced']
+    'kernel': ['linear', 'poly', 'rbf']
     }
+
     print(f"Parameter Search Started (Using all CPU Cores in parallel): {datetime.now()}")
     grid_search = GridSearchCV(classifier, param_grid, cv=5, n_jobs=-1)
     grid_search.fit(X_train, y_train)
     print(f"Parameter Search Completed: {datetime.now()}")
 
     params = grid_search.best_params_
-    print(params)
+    print(f'Beste Parameter: {params}')
 
    
-    classifier = SVC(C=params['C'], kernel=params['kernel'], gamma=params['gamma'], degree=params['degree'], class_weight=params['class_weight'])
+    classifier = SVC(C=params['C'], kernel=params['kernel'])
     print("Training started")
     classifier.fit(X_train, y_train)
     print("Training finished")
@@ -183,6 +186,25 @@ def predict_single_gender(classifier, audio_path):
         print(e)
         return("Prediction failed", False)
 
+def predict_single_speaker(classifier, audio_path):
+    '''
+    Predict for a single sample who the speaker is
+    :param classifier: a prerained classifier 
+    :param audio_path: path to the audio file to be classified
+    :return: If no exception is raised -> A String with the name of the sample and the predicted speaker, True
+             If an exception is raised -> A String with a short notice of the failed prediction, False
+    '''
+    new_mfcc_features,_,_ = extract_mfcc(audio_path)
+    try:
+        predicted_label1 = classifier.predict([new_mfcc_features])
+        return(f"{audio_path} is a sample of a {predicted_label1[0]}", True)
+  
+    except Exception as e:
+        print(e)
+        return("Prediction failed", False)
 
 
-train_speaker_classification("samples\commonvoice\info\Filtered.xlsx", "samples/commonvoice/")   
+#model1 = save_classifier(train_speaker_classification("samples\commonvoice\info\Filtered.xlsx", "samples/commonvoice/"))  
+#print(model1)
+classifier = load_classifier("svc_model10_07_2023_21_08.jl", from_models=True)
+print(predict_single_speaker(classifier, "samples\cloned\Sample_Nils_1.wav"))
